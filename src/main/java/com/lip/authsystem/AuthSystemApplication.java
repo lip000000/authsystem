952335152acddab1b0cf40387bc75054643cd6fc
lip000000 <875902420@qq.com>
2020-09-16 11:47:11 +0800

    11

--- src/main/java/com/lip/authsystem/AuthSystemApplication.java
+++ src/main/java/com/lip/authsystem/AuthSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthSystemApplication {
 
+    //文件测试
     public static void main(String[] args) {
         SpringApplication.run(AuthSystemApplication.class, args);
     }
